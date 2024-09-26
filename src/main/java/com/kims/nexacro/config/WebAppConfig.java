92ebd160648a5bcedd0b648099ff02b2ff6e2e1a
nkims702 <80798564+nkims702@users.noreply.github.com>
2024-09-26 20:38:21 +0900

    0926

--- src/main/java/com/kims/nexacro/config/WebAppConfig.java
+++ src/main/java/com/kims/nexacro/config/WebAppConfig.java
@@ -1,17 +1,11 @@
 package com.kims.nexacro.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 @Configuration
